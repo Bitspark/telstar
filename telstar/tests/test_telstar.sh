fcf72e864d2c3e0ec464225f53bbeeb8045da8bb
Kai Richard Koenig <kai.richard.koenig@gmail.com>
2019-06-28 11:37:12 +0200

    Make the test script more robust and also test that we do not have pending messages

--- telstar/tests/test_telstar.sh
+++ telstar/tests/test_telstar.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
-
-# This script is used to verify/test a assumpations and guaranteesabout the system. 
-# 
-# Todos: 
+# This script is used to verify/test a assumpations and guaranteesabout the system.
+#
+# Todos:
 #   * kill redis in the process
 #   * kill mysql in the process
 #   * use more fuzzing - meaning kill and start processes more randomly to simulate failure
-#   * use this inside a CI
 #   * improve the sleeping, maybe look at how we can wait until all pending messages are done and then make final diff.
 
-
-readonly SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-function finish() {
-    jobs -p | xargs kill
-}
-
-# Upon exit kill all child procceses
-trap finish SIGINT
-trap finish EXIT
-
 # Configuration
-export STREAM_NAME="${STREAM_NAME:-test}"
+export STREAM_NAME="${STREAM_NAME:-mystream}"
 export GROUP_NAME="${GROUP_NAME:-validation}"
 
 export SLEEPINESS="${SLEEPINESS:-10}"
@@ -35,89 +22,118 @@ export DATABASE="${DATABASE:-mysql://root:root@127.0.0.1:3306/test}"
 
 export PYTHONPATH="${PYTHONPATH}:${SCRIPTPATH}../}"
 
+readonly SCRIPTPATH="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
+
+function finish() {
+    jobs -p | xargs kill
+}
+
+function kill_childs_and_exit() {
+    echo "Attempting to kill all childs"
+    echo "..."
+    pkill -P $$
+    echo "ok, bye"
+    exit 1
+}
+
+# Upon exit kill all child procceses
+trap finish SIGINT
+trap finish EXIT
+
+# TRAP CTRL-C and kill all childs
+trap kill_childs_and_exit INT
+
 if [ -x "$(command -v redis-cli)" ]; then
     # Clear redis only if available
     echo "flushing"
-    redis-cli -n $REDIS_DB FLUSHDB 
+    redis-cli -n $REDIS_DB FLUSHDB
 fi
 
+main() {
 
-# Start the first customer with `create` which drops and creates the needed tables in mysql
-CONSUMER_NAME=1 python $SCRIPTPATH/test_consumer.py setup &
-CONSUMER_1=$! # This saves the PID of the last command - which we can use to `kill` the process later
+    # Start the first customer with `create` which drops and creates the needed tables in mysql
+    CONSUMER_NAME=1 python $SCRIPTPATH/test_consumer.py setup &
+    CONSUMER_1=$! # This saves the PID of the last command - which we can use to `kill` the process later
 
-# What for the creation to be done
-sleep 2
+    # What for the creation to be done
+    sleep 2
 
-# Start more consumers
-CONSUMER_NAME=2 python $SCRIPTPATH/test_consumer.py &
-CONSUMER_2=$!
+    # Start more consumers
+    CONSUMER_NAME=2 python $SCRIPTPATH/test_consumer.py &
+    CONSUMER_2=$!
 
-CONSUMER_NAME=3 python $SCRIPTPATH/test_consumer.py &
-CONSUMER_3=$!
+    CONSUMER_NAME=3 python $SCRIPTPATH/test_consumer.py &
+    CONSUMER_3=$!
 
-# Create a producer that double sends messages because it does not delete them after sending. 
-KEEPEVENTS=1 RANGE_FROM=1 RANGE_TO=101 python $SCRIPTPATH/test_producer.py setup create &
-PRODUCER_1=$!
+    # Create a producer that double sends messages because it does not delete them after sending.
+    KEEPEVENTS=1 RANGE_FROM=1 RANGE_TO=101 python $SCRIPTPATH/test_producer.py setup create &
+    PRODUCER_1=$!
 
-sleep 2
+    sleep 2
 
-# Create another producer that keeps double sending
-KEEPEVENTS=1 python $SCRIPTPATH/test_producer.py &
-PRODUCER_2=$!
+    # Create another producer that keeps double sending
+    KEEPEVENTS=1 python $SCRIPTPATH/test_producer.py &
+    PRODUCER_2=$!
 
-# Since CONSUMER_1 is in the background in already has consumed some messages from the stream
-kill $CONSUMER_1 
+    # Since CONSUMER_1 is in the background in already has consumed some messages from the stream
+    kill $CONSUMER_1
 
-# Wait some more for  CONSUMER_2 and CONSUMER_3 to process more data.
-sleep 2
+    # Wait some more for  CONSUMER_2 and CONSUMER_3 to process more data.
+    sleep 2
 
-# Let's kill the producer while it is sending
-kill $PRODUCER_1
-kill $PRODUCER_2
+    # Let's kill the producer while it is sending
+    kill $PRODUCER_1
+    kill $PRODUCER_2
 
-# Now we restart CONSUMER_1
-CONSUMER_NAME=1 python $SCRIPTPATH/test_consumer.py &
-CONSUMER_1=$!
+    # Now we restart CONSUMER_1
+    CONSUMER_NAME=1 python $SCRIPTPATH/test_consumer.py &
+    CONSUMER_1=$!
 
-# Let all consumers process a bit more data
-sleep 5
+    # Let all consumers process a bit more data
+    sleep 5
 
+    # Kill more consumers
+    kill $CONSUMER_1
+    kill $CONSUMER_3
 
-# Kill more consumers
-kill $CONSUMER_1
-kill $CONSUMER_3
+    # Oops all consumers are dead now
+    kill $CONSUMER_2
 
-# Oops all consumers are dead now
-kill $CONSUMER_2
+    # Create another producer that generates the rest of the data but now also delete what was send already
+    RANGE_FROM=101 RANGE_TO=201 python $SCRIPTPATH/test_producer.py create &
+    PRODUCER_3=$!
 
-# Create another producer that generates the rest of the data but now also delete what was send already
-RANGE_FROM=101 RANGE_TO=201 python $SCRIPTPATH/test_producer.py create &
-PRODUCER_3=$!
+    # Start a new one
+    CONSUMER_NAME=4 python $SCRIPTPATH/test_consumer.py &
+    CONSUMER_4=$!
 
+    # Wait for `CONSUMER_4` to process all data
+    sleep 30
+    kill $CONSUMER_4
+    kill $PRODUCER_3
 
-# Start a new one
-CONSUMER_NAME=4 python $SCRIPTPATH/test_consumer.py &
-CONSUMER_4=$!
+    # Restart `CONSUMER_4` and kill it later
+    CONSUMER_NAME=4 python $SCRIPTPATH/test_consumer.py &
+    CONSUMER_4=$!
 
-# Wait for `CONSUMER_4` to process all data
-sleep 30
-kill $CONSUMER_4
-kill $PRODUCER_3
+    sleep 30
+    kill $CONSUMER_4
 
-# Restart `CONSUMER_4` and kill it later
-CONSUMER_NAME=4 python $SCRIPTPATH/test_consumer.py &
-CONSUMER_4=$!
+    # Create a new consumer group that reads everything from the beginning of time.
+    SLEEPINESS=1 CONSUMER_NAME=4 GROUP_NAME=validation2 python $SCRIPTPATH/test_consumer.py &
+    NEW_CONSUMER=$!
 
-sleep 30
-kill $CONSUMER_4
+    sleep 10
+    kill $NEW_CONSUMER
 
-# Create a new consumer group that reads everything from the beginning of time.
-SLEEPINESS=1 CONSUMER_NAME=4 GROUP_NAME=validation2 python $SCRIPTPATH/test_consumer.py &
-NEW_CONSUMER=$!
+    # Verify that the database hold the expected records
+    python $SCRIPTPATH/_dbcontent.py | diff $SCRIPTPATH/expected.txt - || exit 1
 
-sleep 10 
-kill $NEW_CONSUMER
-
-# Verify that the database hold the expected records
-python  $SCRIPTPATH/_dbcontent.py | diff $SCRIPTPATH/expected.txt - || exit 1
+    # Makre sure we do not have pending message left
+    test $(redis-cli -n 10 --csv XPENDING telstar:stream:$STREAM_NAME validation) == "0,NIL,NIL,NIL" || exit 1
+    test $(redis-cli -n 10 --csv XPENDING telstar:stream:$STREAM_NAME validation2) == "0,NIL,NIL,NIL" || exit 1
+}
+main
