bd92677b7fb9afda5d56791fb140f536bdc10aef
Kai Richard König <kai.richard.koenig@gmail.com>
2020-06-05 07:47:01 +0200

    Make name in logs clearer by wrapping them in single qoutes

--- telstar/consumer.py
+++ telstar/consumer.py
@@ -60,7 +60,7 @@ class MultiConsumer(object):
         try:
             self.link.xgroup_create(stream_name, self.group_name, mkstream=True, id="0")
         except redis.exceptions.ResponseError:
-            log.debug(f"Group: {self.group_name} for Stream: {stream_name} already exists")
+            log.debug(f"Group: {self.group_name} for Stream: '{stream_name}' already exists")
 
     # In consumer groups, consumers can disappear, when they do they can leave non ack'ed message
     # which we want to claim and be delivered to a new consumer
@@ -73,7 +73,7 @@ class MultiConsumer(object):
         #  'consumers': [{'name': b'cg-userSignUp.1', 'pending': 10}]}
         # Nothing to do
         if pending_info["pending"] == 0:
-            log.debug(f"Stream: {stream_name} in Group: {self.group_name} has no pending messages")
+            log.debug(f"Stream: '{stream_name}' in Group: '{self.group_name}' has no pending messages")
             return
         # Get all messages ids within that range and select the ones we want to claim and claim them
         # But only if they are pending for more than 20secs.
@@ -94,9 +94,9 @@ class MultiConsumer(object):
 
         # It might be cheaper to claim *and* receive the message so we can work on them directly
         # w/o catching up through the history with the potential of a lot of already seen keys.
-        log.debug(f"Stream: {stream_name} in Group: {self.group_name} claiming: {len(messages_to_claim)} message(s)")
+        log.debug(f"Stream: '{stream_name}' in Group: '{self.group_name}' claiming: {len(messages_to_claim)} message(s)")
         claimed_messages = self.link.xclaim(stream_name, self.group_name, self.consumer_name, self.claim_the_dead_after, messages_to_claim, justid=True)
-        log.debug(f"Stream: {stream_name} in Group: {self.group_name} claimed: {len(messages_to_claim)} message(s)")
+        log.debug(f"Stream: '{stream_name}' in Group: '{self.group_name}' claimed: {len(messages_to_claim)} message(s)")
         return claimed_messages
 
     # We claim the message from other dead/non-responsive consumers.
@@ -114,7 +114,7 @@ class MultiConsumer(object):
                 next_after_seen = increment_msg_id(last_seen[stream_name])
                 last_seen[stream_name] = min([before_earliest, next_after_seen])
         # Read all message for the past up until now.
-        log.info(f"Stream: {', '.join(last_seen)} in Group: {self.group_name} as Consumer: {self.consumer_name} reading past messages")
+        log.info(f"Stream: {', '.join(last_seen)} in Group: '{self.group_name}' as Consumer: '{self.consumer_name}' reading past messages")
         self.catchup(last_seen)
 
     # This is the main loop where we start from the history
@@ -129,7 +129,7 @@ class MultiConsumer(object):
         self.transfer_and_process_stream_history(self.streams)
         # With our history processes we can now start waiting for new message to arrive `>`
         config = {k: ">" for k in self.streams}
-        log.info(f"Stream: {', '.join(self.streams)} in Group: {self.group_name} as Consumer: {self.consumer_name} reading pending message or waiting for new")
+        log.info(f"Stream: {', '.join(self.streams)} in Group: '{self.group_name}' as Consumer: '{self.consumer_name}' reading pending message or waiting for new")
         self.read(config, block=self.block)
 
     def get_last_seen_id(self, stream_name: str) -> bytes:
@@ -143,7 +143,7 @@ class MultiConsumer(object):
     #    the UUID for 14 days
     # 3. Acknowledge the message to meaning that we have processed it
     def acknowledge(self, msg: Message, stream_msg_id: bytes) -> None:
-        log.debug(f"Stream: telstar:stream:{msg.stream} in Group: {self.group_name} acknowledging Message: {msg.msg_uuid} - {stream_msg_id}")
+        log.debug(f"Stream: telstar:stream:{msg.stream} in Group: '{self.group_name}' acknowledging Message: {msg.msg_uuid} - {stream_msg_id}")
         check_point_key = self._checkpoint_key(f"telstar:stream:{msg.stream}")
         seen_key = self._seen_key(msg)
         # Execute the following statments in a transaction e.g. redis speak `pipeline`
